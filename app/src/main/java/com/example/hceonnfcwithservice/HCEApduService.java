86b4c391b974f75f8d4fc0e06e54fee6a8c155a9
rgranoszewsk <rafal.granoszewski@zalando.pl>
2023-11-04 18:39:34 +0100

    Last commit

--- app/src/main/java/com/example/hceonnfcwithservice/HCEApduService.java
+++ app/src/main/java/com/example/hceonnfcwithservice/HCEApduService.java
@@ -6,9 +6,38 @@ import android.os.Bundle;
 public class HCEApduService extends HostApduService {
     @Override
     public byte[] processCommandApdu(byte[] apdu, Bundle extras) {
-       return new byte[0];
+        // Verify that APDU is a SELECT command compatible with one of your AIDs.
+        if (isSelectAidApdu(apdu)) {
+            // Return some numeric variable as the answer.
+            byte[] response = convertNumberToResponse(12345);
+            return concatArrays(response, new byte[]{(byte) 0x90, (byte) 0x00});
+        } else {
+            // If it's not a SELECT command, return the status word 'unknown'.
+            return new byte[]{(byte) 0x6F, (byte) 0x00};
+        }
     }
+
     @Override
     public void onDeactivated(int reason) {
     }
+
+    private boolean isSelectAidApdu(byte[] apdu) {
+        // Check if APDU is via SELECT.
+        // (Here you should check that the APDU matches the format of the SELECT command and that the AID is one of your AIDs).
+        return true;
+    }
+
+    private byte[] convertNumberToResponse(int number) {
+        // Convert the number to the appropriate APDU response format.
+        // (Here you should convert the number to an appropriate format, such as a string of bytes).
+        return new byte[]{(byte) number};
+    }
+
+    private byte[] concatArrays(byte[] first, byte[] second) {
+        byte[] result = new byte[first.length + second.length];
+        System.arraycopy(first, 0, result, 0, first.length);
+        System.arraycopy(second, 0, result, first.length, second.length);
+        return result;
+    }
 }
+
